1f28eb7d938220dbfe2a59b6826f37ebd61974d1
rogisg <gogalebedev91@gmail.com>
2024-11-16 20:39:40 +0300

    6.cpp

---
+++ 6.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int diffWordsCounter(const string& str) {
+    vector<string> v;
+    string w;
+
+    for (size_t i = 0; i < str.size(); ++i) {
+        char c = str[i];
+        if (c != ' ' && c != ',' && c != '.') {
+            w += c;
+        } else {
+            if (w.size() > 0) {
+                bool u = true;
+                for (const auto& x : v) {
+                    if (x == w) {
+                        u = false;
+                        break;
+                    }
+                }
+                if (u) {
+                    v.push_back(w);
+                }
+                w = "";
+            }
+        }
+    }
+
+    if (w.size() > 0) {
+        bool u = true;
+        for (const auto& x : v) {
+            if (x == w) {
+                u = false;
+                break;
+            }
+        }
+        if (u) {
+            v.push_back(w);
+        }
+    }
+
+    return v.size();
+}
